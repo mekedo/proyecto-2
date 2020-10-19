d652aac1f755e1644e1be48160ec28f5fa8d2b74
mekedo <71676113+mekedo@users.noreply.github.com>
2020-10-19 08:55:36 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+//import fondopng from './imagen/fondo/fondopng.PNG';
+
+
+function Titulo () {
+  return (
+    <div id ="titulo"> Aquazone </div>
+  )
+}
+
+
+function App() {
+  return (
+    <div>
+       Redes sociales <Titulo/>
+        </div>
+  )
+}  
+
+
+export default App;
